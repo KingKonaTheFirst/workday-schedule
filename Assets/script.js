65183be569ccf8331c1a04e986ee2c354f583713
Kona <landon.deroin@gmail.com>
2023-07-11 19:24:45 -0400

    added local saved data to show back up in the js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,40 @@
 var date = dayjs().format("dddd MMMM D");
 console.log(date);
 
-
 // check the time and adds/changes the class in the html to change the color of the blocks
-$(function (){
+$(function () {
   let hour = dayjs().hour();
-  
-    $('.time-block').each(function(){
-    var currentHourEl = $(this).attr("id").split("-")[1]
-      if (hour < currentHourEl){
-        $(this).addClass("future")
-        } else if(hour == currentHourEl){
-          $(this).addClass("present")
-            }else{
-              $(this).addClass("past")
-    }})
 
-});
-
-$(document).ready(function () {
-  // listen for save button clicks
-  $('.saveBtn').on('click', function () {
+  $(".time-block").each(function () {
+    var currentHourEl = $(this).attr("id").split("-")[1];
+    if (hour < currentHourEl) {
+      $(this).addClass("future");
+    } else if (hour == currentHourEl) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+ 
+//  allows the save button to function at saves textbox to local
+  $(".saveBtn").on("click", function () {
     // get nearby values
-    var value = $(this).siblings('.description').val();
-    var parent = $(this).parent().attr('id');
+    var value = $(this).siblings(".description").val();
+    var parent = $(this).parent().attr("id");
 
     // save in localStorage
     localStorage.setItem(parent, value);
   });
+// pulls desc from local storage and displays into the box
+$(".description").each(function(){
+  var parentId = $(this).parent().attr("id");
+  var text = localStorage.getItem(parentId);
+  $(this).val(text);
+
+})
+
 });
 
 
 
-//************************************************************************************************************************************************* */
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page
