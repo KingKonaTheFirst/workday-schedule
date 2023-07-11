bcfb42e8a3964d342fef1feabb50bf2ff4e6b616
Kona <landon.deroin@gmail.com>
2023-07-11 19:43:37 -0400

    added clock to the header in java

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 var date = dayjs().format("dddd MMMM D");
-console.log(date);
+// console.log(date);
 
 // check the time and adds/changes the class in the html to change the color of the blocks
 $(function () {
@@ -15,8 +15,8 @@ $(function () {
       $(this).addClass("past");
     }
   });
- 
-//  allows the save button to function at saves textbox to local
+
+  //  allows the save button to function at saves textbox to local
   $(".saveBtn").on("click", function () {
     // get nearby values
     var value = $(this).siblings(".description").val();
@@ -25,16 +25,13 @@ $(function () {
     // save in localStorage
     localStorage.setItem(parent, value);
   });
-// pulls desc from local storage and displays into the box
-$(".description").each(function(){
-  var parentId = $(this).parent().attr("id");
-  var text = localStorage.getItem(parentId);
-  $(this).val(text);
-
-})
-
+  // pulls desc from local storage and displays into the box
+  $(".description").each(function () {
+    var parentId = $(this).parent().attr("id");
+    var text = localStorage.getItem(parentId);
+    $(this).val(text);
+  });
+  
+  let displayedDate = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(displayedDate);
 });
-
-
-
-
